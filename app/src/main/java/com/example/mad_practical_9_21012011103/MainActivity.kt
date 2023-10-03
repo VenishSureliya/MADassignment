d13e652aea86ef07448e831afdd801003a554414
student <student@gnu.ac.in>
2023-10-03 08:52:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_9_21012011103/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mad_practical_9_21012011103
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var alarmAnimationDrawable: AnimationDrawable
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val imagealarm=findViewById<ImageView>(R.id.imageView)
+        imagealarm.setBackgroundResource(R.drawable.alarm_animaton_list)
+        alarmAnimationDrawable=imagealarm.background as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        if (hasFocus){
+            alarmAnimationDrawable.start()
+        }
+        else{
+            alarmAnimationDrawable.stop()
+        }
+        super.onWindowFocusChanged(hasFocus)
+    }
+}
